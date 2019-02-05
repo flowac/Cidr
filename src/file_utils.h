52c21af5660814085cfffa37d0c847f865ea0258
Alan Cai <alanalt5@live.ca>
2019-02-05 15:06:44 -0500

    Tried to reduce memory usage. u32p_pop has serious issues

--- src/file_utils.h
+++ src/file_utils.h
@@ -26,11 +26,10 @@ typedef struct u32_u32 {
 	uint32_t *data;//Line numbers
 } u32_u32;
 
-typedef struct u32_str {
-	uint32_t  key; //Line number of strings
-	uint32_t  len;
-	char    **data;//Strings
-} u32_str;
+typedef struct u32_pair {
+	uint32_t left;
+	uint32_t right;
+} u32_pair;
 
 uint32_t fsize(FILE *fp)
 {
@@ -116,68 +115,44 @@ uint32_t u32_pop(u32_u32 *arr, uint32_t len, uint64_t val)
 RETURN:	return ret;
 }
 
-uint32_t str_find(const u32_str *arr, const uint32_t len, const uint32_t val)
+uint32_t u32p_find(const u32_pair *arr, const uint32_t len, const uint32_t val)
 {
 	uint32_t left = 0, mid = MAX_U32, right = len - 1;
 	if (!arr || len == 0) goto RETURN;
 	while (left <= right) {
 		mid = (left + right) / 2;
-		if (arr[mid].key < val) left = mid + 1;
-		else if (arr[mid].key > val) {
-			if (mid == 0) goto FAILED;
-			right = mid - 1;
-		} else  goto RETURN;
+		if (val < arr[mid].left)      right = mid > 0 ? mid - 1 : 0;
+		else /*if (val > arr[mid].left)*/ left  = ++mid;
 	}
-FAILED:	mid ^= MAX_U32;
 RETURN:	return mid;
 }
 
-u32_str *str_free(u32_str **ptr, uint32_t len)
+u32_pair *u32p_insert(u32_pair *arr, uint32_t *len, const uint32_t left, const uint32_t right)
 {
-	if (!*ptr) goto RETURN;
-	for (uint32_t i = 0; i < len; i++) free((*ptr)[i].data);
-	free(*ptr);
-RETURN:	return 0;
-}
-
-u32_str *str_insert(u32_str *arr, uint32_t *len, const char *str, const uint32_t pos)
-{
-	char *str2 = malloc(strlen(str) + 1);
-	u32_str *arr2 = arr;
-	uint32_t flag;
-
-	if (!str2) goto FAILED;
-	if (MAX_U28 < (flag = str_find(arr2, *len, pos))) {
-		if (!(arr2 = realloc(arr2, sizeof(u32_u32) * (*len + 1)))) goto FAILED;
-		flag ^= MAX_U32;
-		if (flag != *len) {
-			if (arr2[flag].key < pos) flag++;
-			memmove(arr2 + flag + 1, arr2 + flag, sizeof(u32_str) * (*len - flag));
-		}
-		arr2[flag].key  = pos;
-		arr2[flag].len  = 0;
-		arr2[flag].data = 0;
-		*len += 1;
+	u32_pair *arr2 = arr;
+	uint32_t flag = u32p_find(arr2, *len, left);
+	if (flag == MAX_U32) flag = 0;
+	if (!(arr2 = realloc(arr2, sizeof(u32_pair) * (*len + 1)))) {
+		free(arr2);
+		arr2 = 0;
+		goto RETURN;
 	}
-	if (!(arr2[flag].data = realloc(arr2[flag].data, sizeof(char *) * (arr2[flag].len + 1)))) goto FAILED;
-	strcpy(str2, str);
-	arr2[flag].data[arr2[flag].len] = str2;
-	arr2[flag].len++;
-	goto RETURN;
-FAILED:	arr2 = str_free(&arr, *len);
-	if (str2) free(str2);
+	if (flag < *len) memmove(arr2 + flag + 1, arr2 + flag, sizeof(u32_pair) * (*len - flag));
+	arr2[flag].left = left;
+	arr2[flag].right = right;
+	*len += 1;
 RETURN:	return arr2;
 }
 
-char *str_pop(u32_str *arr, uint32_t len, uint32_t val)
+uint32_t u32p_pop(u32_pair *arr, uint32_t *len, uint32_t left)
 {
-	uint32_t pos = str_find(arr, len, val);
-	char *str = 0;
-	if (MAX_U28 < pos)     goto RETURN;
-	if (arr[pos].len == 0) goto RETURN;
-	str = arr[pos].data[0];
-	if (--arr[pos].len > 0) memmove(arr[pos].data, arr[pos].data + 1, sizeof(char *) * arr[pos].len);
-RETURN:	return str;
+	uint32_t right = MAX_U32;
+	if (*len == 0)           goto RETURN;
+	if (arr[0].left != left) goto RETURN;
+	right = arr[0].right;
+	*len -= 1;
+	if (*len > 0) memmove(arr, arr + 1, sizeof(u32_pair) * *len);
+RETURN:	return right;
 }
 
 uint8_t *sha512(const char *name)
@@ -201,6 +176,7 @@ RETURN:	if (fp)     fclose(fp);
 	return md_val;
 }
 
+//TODO: create a simpler message digest function with acceptable collision rate
 uint64_t shake128(const char *data, uint32_t len)
 {
 	EVP_MD_CTX *md_ctx = 0;
@@ -218,23 +194,32 @@ RETURN:	if (md_ctx) EVP_MD_CTX_free(md_ctx);
 
 void diff_txt(const char *left_file, const char *right_file, FILE *out)
 {
-	char buf[BUF_1K] = {0}, *tmp;
+	char buf[BUF_1K] = {0};
 	FILE *left = 0, *right = 0;
-	u32_u32  *chksum = 0;
-	u32_str  *tmpstr = 0;
-	uint32_t  flag, i, left_len = 0, *lookup = 0;
-	uint32_t  chksum_len = 0, right_len = 0, tmpstr_len = 0;
-	uint64_t  sum;
+	u32_u32 *chksum = 0;
+	//The position where lines needed to be inserted on the left side is recorded in *add
+	u32_pair *add = 0;
+	//Line number offset for the right side is stored in *offset
+	int32_t *offset = 0;
+	//Lookup table stores whether line on left side is removed at most significant bit
+	//Then it stores the line number where right side is added (WRT left) at lower 31 bits
+	uint32_t flag, i, left_len = 0, *lookup = 0;
+	uint32_t chksum_len = 0, right_len = 0, add_len = 0;
+	uint64_t sum;
 
 	if (!(left  = fopen(left_file,  "r"))) goto RETURN;
 	if (!(right = fopen(right_file, "r"))) goto RETURN;
 	//Get checksum of each line
 	for (; fgets(buf, BUF_1K, right); *buf = 0, right_len++) {
 		if (!(lookup = realloc(lookup, sizeof(uint32_t) * (right_len + 1)))) goto RETURN;
+		if (!(offset = realloc(offset, sizeof(uint32_t) * (right_len + 2)))) goto RETURN;
 		lookup[right_len] = MAX_U31;
+		offset[right_len+1] = ftell(right);
 		sum = shake128(buf, strlen(buf));
 		if (!(chksum = u32_insert(chksum, &chksum_len, sum, right_len))) goto RETURN;
 	}
+puts("done right");
+	offset[0] = 0;
 	for (; fgets(buf, BUF_1K, left); *buf = 0, left_len++) {
 		if (left_len > right_len) if (!(lookup = realloc(lookup, sizeof(uint32_t) * (left_len + 1)))) goto RETURN;
 		sum = shake128(buf, strlen(buf));
@@ -244,30 +229,33 @@ void diff_txt(const char *left_file, const char *right_file, FILE *out)
 			lookup[left_len] &= MAX_U31;
 		} else  lookup[left_len] |= NOT_U31;
 	}
+puts("done left");
 	fseek( left, 0L, SEEK_SET);
-	fseek(right, 0L, SEEK_SET);
 	//Process and print output
-	for (i = flag = 0; fgets(buf, BUF_1K, right) && i < right_len; i++) {
+	for (i = flag = 0; i < right_len; i++) {
 		if ((lookup[i] & MAX_U31) < MAX_U31) {
 			flag = lookup[i] & MAX_U31;
 			continue;
 		}
-		if (!(tmpstr = str_insert(tmpstr, &tmpstr_len, buf, flag))) goto RETURN;
+		if (!(add = u32p_insert(add, &add_len, flag, i))) goto RETURN;
 	}
+puts("done pre process");
 	for (i = 0; fgets(buf, BUF_1K, left) && i < left_len; i++) {
 		if ((lookup[i] >> 31) & 1) fprintf(out, C_RED "--%5d %s" C_STD, i + 1, buf);
 //		else           fprintf(out, "  %5d %s", i + 1, buf);
-		while ((tmp = str_pop(tmpstr, tmpstr_len, i))) {
-			fprintf(out, C_GRN "++%5d %s" C_STD, i + 1, tmp);
-			free(tmp);
+		while (MAX_U32 != (flag = u32p_pop(add, &add_len, i))) {
+			fseek(right, offset[flag], SEEK_SET);
+			fgets(buf, BUF_1K, right);
+			fprintf(out, C_GRN "++%5d %s" C_STD, i + 1, buf);
 		}
 	}
 	printf("Line count: left = %d, right = %d\n", left_len, right_len);
 RETURN:	if (left)   fclose(left);
 	if (right)  fclose(right);
+	if (add)    free(add);
 	if (lookup) free(lookup);
+	if (offset) free(offset);
 	if (chksum) u32_free(&chksum, chksum_len);
-	if (tmpstr) str_free(&tmpstr, tmpstr_len);
 }
 
 char diff_bin(const char *left, const char *right) {
